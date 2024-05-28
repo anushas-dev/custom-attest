9421ef1392838df65c762f1ba0271a434f4c6ec6
Anusha <95580494+anushas-dev@users.noreply.github.com>
2024-05-28 23:07:27 +0530

    added log levels

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,9 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule , {
-    logger: ['error', 'warn'],
+    logger: ['log', 'error', 'warn', 'fatal'],
+    autoFlushLogs: false,
+
   });
   await app.listen(3000);
 }
